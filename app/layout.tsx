999b3e8215e8be8b11e53bac47d7ae1c96e37124
Yusuf-98 <yusuf.smg@gmail.com>
2026-07-05 15:05:22 +0700

    hapus overflowdebugger

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Red_Hat_Display } from 'next/font/google';
 import './globals.css';
-import { OverflowDebugger } from '@/components/debug/OverflowDebugger';
 
 const redHatDisplay = Red_Hat_Display({
   subsets: ['latin'],
@@ -22,8 +21,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className={`${redHatDisplay.variable}`}>
-      {children}
-      <OverflowDebugger />
       <body className='antialiased'>{children}</body>
     </html>
   );
